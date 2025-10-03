af01b915ca040517de5ff0c3f33e8fd005acd980
cultist <75032462+dairycultist@users.noreply.github.com>
2025-10-03 17:03:42 -0400

    Add files via upload

--- dm.mjs
+++ dm.mjs
@@ -6,18 +6,31 @@ let persistentMemory = [
 	{
 		"role": "system",
 		"content":
-			`You are describing a homebrew medieval fantasy world. In this world, obesity grants women magical
-			abilities at the expense of physical ability. Men cannot use magic. Speak briefly. Keep descriptions short.`
+			`You are describing a homebrew medieval fantasy world. In this world, extreme obesity grants women
+			magical abilities at the expense of physical ability (priestesses can often barely walk). Magical
+			abilities are rather weak, allowing for only simple spells. Men cannot use magic. Speak briefly.
+			Keep descriptions short.`
 			.replaceAll("\n", " ").replaceAll("\t", "")
 	},
 	{
-		"role": "assistant",
+		"role": "system",
 		"content":
 			`The user, who is a male adventurer, has a travelling party that consists of:
-			- Solara, a severely obese and shy elven priestess.`
+			- Solara, a severely obese and shy elven priestess.
+			Solara knows these spells:
+			- A healing spell which reverses minor wounds`
 			.replaceAll("\n", " ").replaceAll("\t", "")
 	}
 ];
+
+let inventory = [
+	"A shortsword",
+	"A pack of rations",
+	"A pack of rations",
+	"A pack of rations"
+];
+
+// should add money system, silver coins
 
 let sessionMemory = [
 	{
@@ -25,31 +38,44 @@ let sessionMemory = [
 		"content":
 			`You've woken up after a long night's rest in an inn in a small village nestled within a grassy valley.
 			Currently, you and your party member Solara sit around a round table in the common area of the inn.
-			She seems anxious to get going. What do you do?`
+			She seems anxious to get going.`
 			.replaceAll("\n", " ").replaceAll("\t", "")
 	}
 ];
 
-console.log("\x1b[0m" + sessionMemory[0].content);
+console.log("\n\x1b[33m" + sessionMemory[0].content + "\x1b[0m\n");
 
 while (true) {
 
-	console.log("\x1b[32m");
-	let message = question("> ").trim().toLowerCase();
-	console.log("\x1b[0m");
+	let message;
 
-	if (message == "exit") {
-		process.exit(0);
-	}
+	switch (question("1. speak\n2. use\n").trim().toLowerCase()) {
 
-	if (message != "") {
+		case "1":
+		case "speak":
 
-		sessionMemory.push({
-			"role": "user",
-			"content": message
-		});
+			message = question("What do you say? ").trim().toLowerCase();
 
-		console.log(await pollAI());
+			if (message != "") {
+
+				sessionMemory.push({
+					"role": "user",
+					"content": "The user says: " + message
+				});
+
+				console.log("\n\x1b[33m" + await pollAI() + "\x1b[0m\n");
+			}
+			break;
+		
+		case "2":
+		case "use":
+
+			// use what?
+			// use _ in order to...
+
+			// "The user uses _ to: "
+			break;
+		
 	}
 }
 

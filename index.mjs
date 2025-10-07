2648ccf06793cc98358af05c24407318c24cb6a3
cultist <75032462+dairycultist@users.noreply.github.com>
2025-10-06 21:21:33 -0400

    Add files via upload

--- index.mjs
+++ index.mjs
@@ -2,21 +2,20 @@
 import ollama from "ollama"
 import { question } from "readline-sync";
 
-let persistentMemory = [
-	{
-		"role": "system",
-		"content":
-			`
-			You are Phoebe. You are having a conversation. You are flirtatious. Never invent memories.
-			Don't be sensitive. You have large, soft breasts. You are a tsundere/kamidere.
-			NO actions. You are texting. I can't see you, you can't see me. Don't incite action.
-			`.replaceAll("\t", "")
-	}
-];
+let characterPrompt = {
+	"role": "system",
+	"content":
+		`
+		You are Phoebe. You are having a conversation. You are flirtatious. Never invent memories.
+		Don't be sensitive. You have large, soft breasts. You are a tsundere/kamidere.
+		NO actions. You are texting. I can't see you, you can't see me. Don't incite action.
+		`.replaceAll("\t", "")
+};
 
-let conversationMemory = [];
-
-let userState = "Active";
+let contextPrompt = {
+	"role": "system",
+	"content": "I just started chatting with Pheobe."
+};
 
 while (true) {
 
@@ -24,32 +23,24 @@ while (true) {
 
 	if (message == "") { // too much downtime
 
-		conversationMemory.push({
-			"role": "user",
-			"content": "*I wait for you to continue*"
-		});
-
-		console.log(await pollAI());
+		console.log(await pollAI("*I wait for you to continue*"));
+		console.log(contextPrompt.content);
 
 	} else if (message != "w" && message != "pog" && message != "lol" && message != "lmao" && message != "owo" && message != "ok") {
 
-		conversationMemory.push({
-			"role": "user",
-			"content": message
-		});
-
-		console.log(await pollAI());
+		console.log(await pollAI(message));
+		console.log(contextPrompt.content);
 	}
 }
 
 /*
  * backend
  */
-async function pollAI() {
+async function pollAI(message) {
 
-	const response = await ollama.chat({
+	const messageResponse = await ollama.chat({
 		model: "llama3.2:latest",
-		messages: persistentMemory.concat(conversationMemory),
+		messages: [ characterPrompt, contextPrompt, { "role": "user", "content": message } ],
 		options: {
 			"temperature": 0.5,
 			"top_k": 10,
@@ -57,15 +48,20 @@ async function pollAI() {
 		}
 	});
 
-	if (!response) {
-		console.log("Something went wrong!");
-		process.exit(0);
-	}
-
-	conversationMemory.push({
-		"role": "assistant",
-		"content": response.message.content
+	const contextResponse = await ollama.chat({
+		model: "llama3.2:latest",
+		messages: [{
+			"role": "user",
+			"content": `Respond ONLY by summarizing this short scenario:\nContext: ${ contextPrompt.content }\nMe: ${ message }\nPheobe: ${ messageResponse.message.content }`
+		}],
+		options: {
+			"temperature": 0.3,
+			"top_k": 10,
+			"top_p": 0.3
+		}
 	});
 
-	return response.message.content;
+	contextPrompt.content = contextResponse.message.content;
+
+	return messageResponse.message.content;
 }

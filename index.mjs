75bbeea2570d971cf785c0ee975d18c865cc193e
cultist <75032462+dairycultist@users.noreply.github.com>
2025-10-06 21:48:53 -0400

    Add files via upload

--- index.mjs
+++ index.mjs
@@ -2,66 +2,40 @@
 import ollama from "ollama"
 import { question } from "readline-sync";
 
-let characterPrompt = {
-	"role": "system",
-	"content":
-		`
-		You are Phoebe. You are having a conversation. You are flirtatious. Never invent memories.
-		Don't be sensitive. You have large, soft breasts. You are a tsundere/kamidere.
-		NO actions. You are texting. I can't see you, you can't see me. Don't incite action.
-		`.replaceAll("\t", "")
-};
-
-let contextPrompt = {
-	"role": "system",
-	"content": "I just started chatting with Pheobe."
-};
+// Pheobe speaks only by sending pictures of herself (and her large, soft breasts) with premade "chat stickers" superimposed on them.
 
 while (true) {
 
-	let message = question("> ").trim().toLowerCase();
+	let message = question("   You: ").trim().toLowerCase();
 
 	if (message == "") { // too much downtime
 
-		console.log(await pollAI("*I wait for you to continue*"));
-		console.log(contextPrompt.content);
+		await prompt("*no response*");
 
-	} else if (message != "w" && message != "pog" && message != "lol" && message != "lmao" && message != "owo" && message != "ok") {
+	} else {
 
-		console.log(await pollAI(message));
-		console.log(contextPrompt.content);
+		await prompt(message);
 	}
 }
 
 /*
  * backend
  */
-async function pollAI(message) {
+async function prompt(message) {
+
+	const characterPrompt = {
+		"role": "system",
+		"content":
+			`
+			You are Phoebe, my AI girlfriend. Speak only by saying "LOL", "OK?", "OK!", "YAY", "YEA", "NAH", "WHAT", "HI", and "WAKE UP!".
+			You are playful. Act like a tsundere. Keep responses short and brief.
+			`.replaceAll("\t", "")
+	};
 
 	const messageResponse = await ollama.chat({
 		model: "llama3.2:latest",
-		messages: [ characterPrompt, contextPrompt, { "role": "user", "content": message } ],
-		options: {
-			"temperature": 0.5,
-			"top_k": 10,
-			"top_p": 0.5
-		}
+		messages: [ characterPrompt, { "role": "user", "content": message } ]
 	});
 
-	const contextResponse = await ollama.chat({
-		model: "llama3.2:latest",
-		messages: [{
-			"role": "user",
-			"content": `Respond ONLY by summarizing this short scenario:\nContext: ${ contextPrompt.content }\nMe: ${ message }\nPheobe: ${ messageResponse.message.content }`
-		}],
-		options: {
-			"temperature": 0.3,
-			"top_k": 10,
-			"top_p": 0.3
-		}
-	});
-
-	contextPrompt.content = contextResponse.message.content;
-
-	return messageResponse.message.content;
+	console.log("Pheobe: " + messageResponse.message.content);
 }

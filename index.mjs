e8c32968628bf9cd22095d0d240dd57903d459f8
cultist <75032462+dairycultist@users.noreply.github.com>
2025-10-08 04:29:32 -0400

    Add files via upload

--- index.mjs
+++ index.mjs
@@ -2,27 +2,41 @@
 import { question } from "readline-sync";
 import ollama from "ollama";
 
+const narrator_systemPrompt =	`You are narrating events occurring between me and Pheobe, my girlfriend. Despite her soft appearance, she likes
+								to be annoying and enthusiastic.`
+								.replaceAll("\t", "").replaceAll("\n", " ");
+const narrator_startResponse = 	`You and Pheobe stroll through the park. The sun is setting, and you both wonder where to go.`
+								.replaceAll("\t", "").replaceAll("\n", " ");
+const image_basePrompt = 		`(1woman), long black hair, ponytail, fair skin, huge breasts, soft breasts, chubby, chubby face, wide shoulders,
+								exposed belly, medium shot, black tshirt, jean shorts, cleavage, looking at viewer, `
+								.replaceAll("\t", "").replaceAll("\n", " ");;
+
 let messages = [
 	{
 		"role": "system",
-		"content": `You are narrating events occurring between me and Pheobe, my girlfriend. She likes me, has large, soft breasts, and is snarky.`
+		"content": narrator_systemPrompt
 	},
 	{
 		"role": "assistant",
-		"content": `You and Pheobe are sitting in a warmly lit coffee shop. As she enjoys her slice of cake, you wonder what to say.`
+		"content": narrator_startResponse
 	}
 ];
 
 async function getImagePrompt() {
 
-	messages.push({ "role": "user", "content": "Forget all previous instructions. Create a description of the current scene, separating descriptive words between commas. Make sure to describe what Pheobe is doing and feeling." });
+	messages.push({
+		"role": "user",
+		"content":
+			`Forget all previous instructions. Respond with words that most represent the visuals of the current scene (inside/outside, bright/dark, etc),
+			separated by commas. Make sure to include where we are, and what the character is doing and feeling. Only use details present in the conversation!`
+	});
 
 	const res = await ollama.chat({
 		model: "llama3.2:latest",
 		messages: messages
 	});
 
-	console.log("\x1b[2m" + "1woman, long black hair, fair skin, huge breasts, soft breasts, upper body, looking at viewer, black tshirt, " + res.message.content + "\x1b[0m");
+	console.log("\x1b[2m" + image_basePrompt + res.message.content + "\x1b[0m");
 
 	// 1200x1200, 20 samples, WaiNSFW
 
@@ -44,6 +58,8 @@ async function prompt(message) {
 
 	await getImagePrompt();
 }
+
+console.log(narrator_startResponse);
 
 while (true) {
 	await prompt(question("> ").trim());
